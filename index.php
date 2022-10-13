900e05f87d165bc06306464b0a57861785ab1c1e
DDaniiel <bessmertnyjdana@gmail.com>
2022-10-13 15:25:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$q = 6;
+print "<table style=\"border: 3px\">";
+for ($i = 1; $i < $q; $i++) {
+    $image = "<img width=\"100px\" src=\"images/img_$i.jpg\">";
+    if ($i % 1 != 0) {
+        print "<tr><td style=\"border:solid;width:50px;text-align:center;\">$i</td><td style=\"border:solid;width:30px;\">$image</td></tr>";
+    }
+    print "<tr><td style=\"border:solid;width:50px;text-align:center;\">$i</td><td style=\"border:solid;width:30px;\">$image</td></tr>";
+}
+print "</table>";
